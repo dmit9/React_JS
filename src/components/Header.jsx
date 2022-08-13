921f65f9b8beb060501b361a095d8c07c68a3464
dmit9 <devyatin5@gmail.com>
2022-08-13 14:16:43 +0300

    12

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+﻿
+const Header = () => {
+    return <header className="header">
+        <img src="https://i.pinimg.com/originals/0d/cf/b5/0dcfb548989afdf22afff75e2a46a508.jpg"/>
+    </header>
+}
+
+export default Header;
