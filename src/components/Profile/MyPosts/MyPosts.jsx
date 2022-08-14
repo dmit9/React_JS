7b620bf81a6f3950bfff4256514becb4be5f7e7d
dmit9 <devyatin5@gmail.com>
2022-08-14 09:18:41 +0300

    15

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,19 @@
+﻿import s from './MyPosts.module.css';
+import Post from './Post/Post';
+
+const MyPosts = () => {
+    return (<div className={s.content}>
+        My post
+        <div>
+            <textarea></textarea>
+            <button>Add post</button>
+        </div>
+        <div className={s.posts}>
+            <Post/>
+            <Post/>
+            <Post/>
+        </div>
+    </div>)
+}
+
+export default MyPosts;
---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,15 @@
+﻿import s from './Post.module.css';
+
+const Post = () => {
+    return (
+            <div className={s.item}>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRb0lOhuAbflByagOfYUg5R8F1zGO1aN4woA&usqp=CAU'/>
+                post 1
+                <div>
+                <span>like</span>
+                </div>
+            </div>
+    )
+}
+
+export default Post;
