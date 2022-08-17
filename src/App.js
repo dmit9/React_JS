dca1ac69da93fcdd9d55202377d6918f9520cd52
dmit9 <devyatin5@gmail.com>
2022-08-17 22:25:25 +0300

    19

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
 import Profile from './components/Profile/Profile';
+import { Routes, Route } from "react-router-dom";
 
-const App = () => {
-  return(
+function App() {
+  return (
     <div className='app-wrapper'>
-        <Header />
-        <Navbar />
-        <div class='app-wrapper-content'>
-           <Dialogs />
+            <Header />
+            <Navbar />
+            <div class='app-wrapper-content'>
+                <Routes>
+                    <Route path="/dialogs" element={<Dialogs />}/>
+                    <Route path="/profile" element={<Profile />}/>
+                </Routes>
+            </div>
         </div>
-        {/*<Profile /> */}
-    </div>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,10 +3,10 @@ console.log('SSSS'+s);
 const Navbar = () => {
     return <nav className={s.nav}>
     <div className={s.item}>
-        <a>Profile</a>
+        <a href='/profile'>Profile</a>
     </div>
     <div className={`${s.item} ${s.active}`}>
-        <a>Messages</a>
+        <a href='Dialog'>Messages</a>
     </div>
     <div className={s.item}>
         <a>News</a>
