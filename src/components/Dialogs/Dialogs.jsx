2339ed6a8984d6cc5e8f68041a2616c7fa4f858d
dmit9 <devyatin5@gmail.com>
2022-08-18 16:11:06 +0300

    21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,14 +1,34 @@
 ﻿
 import s from './Dialogs.module.css';
 
-const Dialogs = (props) => {
-    return (
-        <div>
-        <div>
-            Dialogs
+const Dialogs = (props) => (
+    <div className={s.dialogs}>
+        <div className={s.dialogsItems}>
+            <div className={s.dialog + ' ' + s.active}>
+                Dima
+            </div>
+            <div className={s.dialog}>
+                Andrey
+            </div>
+            <div className={s.dialog}>
+                Sveta
+            </div>
+            <div className={s.dialog}>
+                Sacha
+            </div>
+            <div className={s.dialog}>
+                Viktor
+            </div>
+            <div className={s.dialog}>
+                Valera
+            </div>
         </div>
+        <div className={s.messages}>
+            <div className={s.message}>Hi</div>
+            <div className={s.message}>Yo</div>
+            <div className={s.message}>Sssss</div>
         </div>
-    )
-}
+    </div>
+)
 
 export default Dialogs;
