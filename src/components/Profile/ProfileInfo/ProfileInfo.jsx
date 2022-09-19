1285281877a41c496953e445a9a97bfbc7788a21
dmit9 <devyatin5@gmail.com>
2022-09-19 22:00:08 +0300

    71

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,6 @@
 ﻿import Preloader from '../../Preloader/Preloader';
 import s from './ProfileInfo.module.css';
+import ProfileStatus from './ProfileStatus'
 
 const ProfileInfo = (props) => {
         if (!props.profile ) {
@@ -7,13 +8,14 @@ const ProfileInfo = (props) => {
         }
     return (
     <div>
-        <div>
+       {/*  <div>
             <img src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"/>
-        </div>
-        <div>{props.profile.userId}</div>
+        </div> */}
         <div className={s.descriptionBlock}>
-            <img src={props.profile.photos.large} />
+        <img src={props.profile.photos.large} />
         </div>
+        <ProfileStatus status={'fffff'}/>
+        <div>ID - {props.profile.userId}</div>
         <div>{props.profile.fullName}</div>
         <div>{props.profile.aboutMe}</div>
         <div>{props.profile.lookingForAJobDescription}</div>
