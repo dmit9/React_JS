bf282f44a3d9b600a85007d74069275d632218cb
dmit9 <devyatin5@gmail.com>
2022-10-10 22:23:58 +0300

    94

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { Routes, Route } from "react-router-dom";
-import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
-import HeaderContainer from './components/Header/HeaderContainer';
 import LoginPage from './components/Login/Login';
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import { connect } from 'react-redux';
 import { initializeApp } from './redux/app-reducer'
 import { compose } from 'redux';
 import Preloader from './components/Preloader/Preloader';
+/* import ProfileContainer from './components/Profile/ProfileContainer'; */
+import HeaderContainer from './components/Header/HeaderContainer';
+/* import DialogsContainer from './components/Dialogs/DialogsContainer'; */
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+/* const HeaderContainer = React.lazy(() => import('./components/Header/HeaderContainer')); */
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends Component {
     componentDidMount() {
@@ -23,7 +26,8 @@ class App extends Component {
             <div className='app-wrapper'>
             <HeaderContainer />
             <Navbar  />
-            <div className='app-wrapper-content'>
+            <Suspense fallback={<div><Preloader /></div>}>
+                <div className='app-wrapper-content'>
                 <Routes>
                     <Route exact path="/dialogs"
                         element={<DialogsContainer/>}/>
@@ -36,8 +40,9 @@ class App extends Component {
                     <Route path="/login*"
                         element={ < LoginPage />}/>
                 </Routes>
-            </div>
                 </div>
+            </Suspense>
+            </div>
           );
 }}
 
